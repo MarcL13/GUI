d18c82727a00960049b2215ba70c30f068254e43
MarcL13 <42978461+MarcL13@users.noreply.github.com>
2019-02-05 14:11:01 -0500

    Add files via upload

---
+++ src/GUIInput.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class GUIInput {
+
+	public static void main(String[] args)
+	{
+		JFrame box = new JFrame();
+		box.setBounds(400,300,450,250);
+		box.setVisible(true);
+		box.setLayout(null);
+		box.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTextField askNum = new JTextField();
+		askNum.setBounds(100,80,200,50);
+		box.add(askNum);
+	}
+
+}
