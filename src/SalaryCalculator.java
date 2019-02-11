6e01a1c7c2a70edec323cb27a07f38eae04a3562
MarcL13 <42978461+MarcL13@users.noreply.github.com>
2019-02-10 19:32:26 -0500

    Add files via upload

--- src/SalaryCalculator.java
+++ src/SalaryCalculator.java
@@ -18,10 +18,12 @@ public class SalaryCalculator {
 		JLabel hourlyRate = new JLabel("Hourly Rate: ");
 		hourlyRate.setBounds(25,25,125,25);
 		myFrame.add(hourlyRate);
+		String hrRate = hourlyRate.getText().toString();
 		
 		JLabel hoursWeek = new JLabel("Hours/Week: ");
 		hoursWeek.setBounds(25,50,125,25);
 		myFrame.add(hoursWeek);
+		String hrWeek = hoursWeek.getText().toString();
 		
 		JTextField ans1 = new JTextField();
 		ans1.setBounds(100,25,125,25);
@@ -35,6 +37,10 @@ public class SalaryCalculator {
 		fullTime.setBounds(25,75,125,25);
 		myFrame.add(fullTime);
 		
+		JLabel calculation = new JLabel("Annual Salary: $");
+		calculation.setBounds(160,100,125,25);
+		myFrame.add(calculation);
+		
 		JButton calculate = new JButton("Calculate");
 		calculate.setBounds(25,100,125,25);
 		myFrame.add(calculate);
@@ -42,13 +48,14 @@ public class SalaryCalculator {
 				{
 					public void actionPerformed(ActionEvent e)
 					{
-						fullTime.isEnabled();
+						boolean isClicked = fullTime.isEnabled();
+						if(isClicked)
+						{
+							
+						}
 					}
 				});
 		
-		JLabel calculation = new JLabel("Annual Salary: $");
-		calculation.setBounds(160,100,125,25);
-		myFrame.add(calculation);
 		
 		
 		
