e56df2302e567ed20fbeae81e37fb9dcdb39fab1
MarcL13 <42978461+MarcL13@users.noreply.github.com>
2019-02-10 19:24:07 -0500

    Add files via upload

--- src/SalaryCalculator.java
+++ src/SalaryCalculator.java
@@ -1,3 +1,8 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -7,20 +12,47 @@ public class SalaryCalculator {
 	public static void main(String[] args) 
 	{
 		JFrame myFrame = new JFrame();
-		myFrame.setBounds(300,200,350,300);
-		myFrame.setVisible(true);
+		myFrame.setBounds(800,300,400,250);
 		myFrame.setLayout(null);
 		
 		JLabel hourlyRate = new JLabel("Hourly Rate: ");
-		hourlyRate.setBounds(25,5,1000,20);
+		hourlyRate.setBounds(25,25,125,25);
 		myFrame.add(hourlyRate);
 		
+		JLabel hoursWeek = new JLabel("Hours/Week: ");
+		hoursWeek.setBounds(25,50,125,25);
+		myFrame.add(hoursWeek);
+		
 		JTextField ans1 = new JTextField();
-		ans1.setBounds(25,5,100,100);
+		ans1.setBounds(100,25,125,25);
 		myFrame.add(ans1);
 		
+		JTextField ans2 = new JTextField();
+		ans2.setBounds(100,50,125,25);
+		myFrame.add(ans2);
+		
+		JCheckBox fullTime = new JCheckBox("Full Time");
+		fullTime.setBounds(25,75,125,25);
+		myFrame.add(fullTime);
+		
+		JButton calculate = new JButton("Calculate");
+		calculate.setBounds(25,100,125,25);
+		myFrame.add(calculate);
+		calculate.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						fullTime.isEnabled();
+					}
+				});
+		
+		JLabel calculation = new JLabel("Annual Salary: $");
+		calculation.setBounds(160,100,125,25);
+		myFrame.add(calculation);
 		
 		
+		
+		myFrame.setVisible(true);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
